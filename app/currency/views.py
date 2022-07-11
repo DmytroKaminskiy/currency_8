7b93ff0768153972f28a462a3ffb663972f68c5c
DmytroKaminskiy <dmytro.kaminskyi92@gmail.com>
2022-07-11 19:54:44 +0300

    homework5

--- app/currency/views.py
+++ app/currency/views.py
@@ -4,7 +4,7 @@ from currency.utils import get_password
 
 
 def generate_password(request):
-    length = int(request.GET.get('length'))
+    length=int(request.GET.get('length'))
 
     password = get_password(length)
     return HttpResponse(password)
