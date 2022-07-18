6e3fadce9df3c3a5dd96f8e62f667541b7fc4059
DmytroKaminskiy <dmytro.kaminskyi92@gmail.com>
2022-07-18 20:10:21 +0300

    lesson7

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,16 +1,44 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 from currency.models import Rate
+
+# context_processor - GLOBAL Context - base.html
+
+def index(request):
+    return render(request, 'index.html')
 
 
 def rate_list(request):
-
-    rates_list = []
-    for rate in Rate.objects.all():
-        html_string = f'ID: {rate.id}, sale: {rate.sale}, buy: {rate.buy} <br>'
-        # breakpoint()
-        rates_list.append(html_string)
-
-    return HttpResponse(str(rates_list))
+    context = {
+        'rate_list': Rate.objects.all(),
+    }
+    return render(request, 'rate_list.html', context=context)  # rate_list.html
 
 
-# ORM, models (table), makemigration + migrate
+def http_response(request):
+    '''
+    1xx - Info
+    2xx - Success
+    3xx - Redirect
+    4xx - Client error
+    5xx - Server error
+
+    200 - OK
+    201 - Created
+    202 - Accepted
+    204	- No Content
+
+    301 - Permanently
+    307	- Temporary Redirect
+
+    400 - Bad Request
+    401 - Unauthorized
+    403 - Forbidden
+    404	- Not Found
+    405	- Method Not Allowed
+
+    500 - Internal Server Error
+    502	- Bad Gateway
+    '''
+    return HttpResponse('EXAMPLE', status=200)
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from currency.views import rate_list
+from currency.views import rate_list, index, http_response
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('', index),
     path('rate/list/', rate_list),
+    path('response/', http_response),
 ]
